a6ef17298ce898459d0447c16016fe1f3859b745
Andreas Furster <andreas.furster@hotmail.com>
2018-12-11 17:44:15 +0100

    Removed unused usings

--- DnsWatcher.ConsoleApp/Program.cs
+++ DnsWatcher.ConsoleApp/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
-using System.Runtime;
-using System.Net.Sockets;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 
